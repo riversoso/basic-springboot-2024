c0faff88d5cf4172e58dd69f2d76f563a7e7dfa1
riversoso <soe0309@naver.com>
2024-06-26 17:35:01 +0900

    11일차 끝

--- sring03/backboard/src/main/java/com/riversoso/backboard/service/BoardService.java
+++ sring03/backboard/src/main/java/com/riversoso/backboard/service/BoardService.java
@@ -26,6 +26,7 @@ import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
@@ -132,6 +133,7 @@ public class BoardService {
             }
         };
     }
+
     // 카테고리 추가된 메서드.
     public Specification<Board> searchBoard(String keyword, Integer cateId) {
         return new Specification<Board>() {
@@ -152,4 +154,20 @@ public class BoardService {
             }
         };
     }
-}
+
+    // 조회수 증가 메서드
+    @Transactional // 조회하면서 업데이트하므로 !
+    public Board hitBoard(Long bno) {
+        // Optional 기능 널 체크
+        Optional<Board> oboard = this.boardRepository.findByBno(bno);
+
+        if (oboard.isPresent()) {
+            Board board = oboard.get();
+            // board.setHit(board.getHit() + 1); // !!!!! 이대로 쓰면 예외발생
+            board.setHit(Optional.ofNullable(board.getHit()).orElse(0) +1);
+            return board;
+        } else {
+            throw new NotFoundException("Board not found!");
+        }
+    }
+} 
--- sring03/backboard/src/test/java/com/riversoso/backboard/BackboardApplicationTests.java
+++ sring03/backboard/src/test/java/com/riversoso/backboard/BackboardApplicationTests.java
@@ -1,13 +1,13 @@
 package com.riversoso.backboard;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackboardApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
